beddb158ba6e9b473ab5af9a597df5f682964ded
Jaber Zarif <jaber.zarif@outlook.fr>
2024-07-12 08:47:25 +0100

    fix messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,21 @@
 const chatForm = document.getElementById('chat-form');
+const charMessages = document.querySelector('.chat-messages');
+
+
+// Get username and room from URL
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+console.log(username, room);
+
 const socket = io();
 
+
+// Message from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
 });
 
 chatForm.addEventListener('submit', (e) => {
@@ -12,3 +25,18 @@ chatForm.addEventListener('submit', (e) => {
     //Emit message to the server
     socket.emit('chatMessage', msg);
 })
+
+
+// Outpt message to DOM
+
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML= `
+        <p class="meta">${message.username} <span>${message.time}</span></p>
+        <p class="text">
+            ${message.text}
+        </p>
+    `;
+    document.querySelector('.chat-messages').appendChild(div); // Pass the div object here
+}
